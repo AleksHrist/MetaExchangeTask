8f9ffb1d82f09405825e2d81d958fd3d433f8fcb
Aleksandar <aleksandar.hristov10@gmail.com>
2021-05-10 21:36:58 +0200

    Initial structure with reading N order books and deserializing them into Net objects

---
+++ MetaExchangeSowaLabs/OrderBookEntity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MetaExchangeSowaLabs
+{
+    public class OrderBookEntity
+    {
+        public DateTime AcqTime { get; set; }
+        public List<BidAskEntity> Bids { get; set; }
+        public List<BidAskEntity> Asks { get; set; }
+    }
+
+    public class BidAskEntity
+    {
+        public OrderEntity Order { get; set; }
+    }
+
+    public class OrderEntity
+    {
+        public Guid? Id { get; set; }
+        public DateTime Time { get; set; }
+        public TypeOfOrderEnum Type { get; set; }
+        public String Kind { get; set; }
+        public Decimal Amount { get; set; }
+        public Decimal Price { get; set; }
+    }
+    
+    public enum TypeOfOrderEnum
+    {
+        Buy,
+        Sell
+    }
+}
---
+++ MetaExchangeSowaLabs/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MetaExchangeSowaLabs
+{
+    class Program
+    {
+        private const string _PathToFile = "order_books_data";
+        private const int _NumberOfRows = 5000;
+        
+        static void Main(string[] args)
+        {
+            Stopwatch st = new Stopwatch();
+            st.Start();
+            //Extract raw data from the file
+            var orderBooks = ExtractNOrderBooks(_PathToFile, _NumberOfRows);
+            
+            //Call the algorithm 
+            var result = MetaExchangeBestPrice(orderBooks, TypeOfOrderEnum.Buy, (decimal) 2.3139);
+            
+            //Print the calculated optimal steps
+            result.ForEach(Console.WriteLine);
+            st.Stop();
+            Console.WriteLine(st.Elapsed);
+        }
+
+        private static List<string> ExtractNOrderBooks(string path, int numberOfRows)
+        {
+            return File.ReadLines(path).Take(numberOfRows).ToList();
+        }
+
+        private static List<string> MetaExchangeBestPrice(List<string> orderBooks, TypeOfOrderEnum typeOfOrder, decimal amountOfBtc)
+        {
+            //remove the initial value so the row can be deserialized
+            var metaExchange = DeserializeOrderBooks(orderBooks);
+
+
+            return new List<string>();
+        }
+
+        private static List<OrderBookEntity> DeserializeOrderBooks(List<string> orderBooks)
+        {
+            var cleanOrderBooks = new List<string>();
+            orderBooks.ForEach(x => { cleanOrderBooks.Add(x.Remove(0, x.IndexOf('{'))); });
+
+            //Serialize the Order books so we can pass it as a parameter for deserialization
+            var customSerializedOrderBooks = "[" + string.Join(",", cleanOrderBooks) + "]";
+
+            //Deserialize to .Net classes
+            List<OrderBookEntity> metaExchange;
+            try
+            {
+                metaExchange = JsonConvert.DeserializeObject<List<OrderBookEntity>>(customSerializedOrderBooks);
+            }
+            catch (JsonException e)
+            {
+                throw new Exception("Couldn't serialize the document! : " + e);
+            }
+
+            return metaExchange;
+        }
+    }
+
+}
